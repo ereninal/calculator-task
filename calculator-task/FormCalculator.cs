5f5c7cf290b0888ef96ab06f3d6330530aa0eadd
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-19 17:16:25 +0300

    Form title is changed.

--- calculator-task/FormCalculator.cs
+++ calculator-task/FormCalculator.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace calculator_task
 {
-    public partial class Form1 : Form
+    public partial class FormCalculator : Form
     {
-        public Form1()
+        public FormCalculator()
         {
             InitializeComponent();
         }
--- calculator-task/Program.cs
+++ calculator-task/Program.cs
@@ -16,7 +16,7 @@ namespace calculator_task
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormCalculator());
         }
     }
 }
