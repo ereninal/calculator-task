8c40209a4e4671afb4e7b5c2cea02b2618ffd3b3
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-19 17:49:07 +0300

    Keys added

---
+++ calculator-task/ClassCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_task
+{
+    public class ClassCalculator
+    {
+        private string[] keys =
+        {
+            "7","8","9","/",
+            "4","5","6","*",
+            "1","2","3","-",
+            "0",",","=","+"
+        };
+
+
+
+    }
+}
